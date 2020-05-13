5f1c413fc5eb1cc6b0fdfbf33544bd863b0b74a8
Woojong Kim <woojong92@gmail.com>
2020-05-13 17:55:55 +0900

    update

---
+++ containers/write/TagBoxContainer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TagBox from "../../components/write/TagBox";
+import { changeField } from "../../modules/write";
+
+
+const TagBoxContainer = () => {
+    const dispatch = useDispatch();
+    const tags = useSelector(state => state.write.tags);
+
+    const onChangeTags = nextTags => {
+        dispatch(
+            changeField({
+                key: "tags",
+                value: nextTags
+            })
+        )
+    }
+
+    return (
+        <TagBox tags={tags} onChangeTags={onChangeTags}/>
+    )
+}
+
+export default TagBoxContainer;
